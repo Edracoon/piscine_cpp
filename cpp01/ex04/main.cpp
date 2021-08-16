2ab71a7804eec07392ceaea2bd75daf3769b029d
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-08-16 16:38:58 +0200

    ex05 done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,13 +6,22 @@ int	main(int ac, char **av)
 {
 	if (ac > 3 && av[1] && av[2] && av[3])
 	{
-		std::string		filename(av[1]);
+		std::string		filein(av[1]);
 		std::string		s1(av[2]);
 		std::string		s2(av[3]);
-
-		std::ifstream	ifs(filename);
-		std::string		file = filename + ".replace";
-		std::ofstream	ofs(file);
+		if (filein.empty() || s1.empty() || s2.empty())
+		{
+			std::cout << "Error: Empty string input" << std::endl;
+			return (0);
+		}
+		std::ifstream	ifs(filein);
+		if (!ifs)
+		{
+			std::cout << "Error: Input file stream doesn't exist" << std::endl;
+			return (0);
+		}
+		std::string		fileout = filein + ".replace";
+		std::ofstream	ofs(fileout);
 
 		std::string		buff;
 		size_t			pos = 0;
