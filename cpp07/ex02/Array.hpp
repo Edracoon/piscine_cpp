ed752c22991c8a48ee4f439ae12ec4b7dfca2114
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-09-06 12:13:24 +0200

    cpp07 done

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -7,9 +7,54 @@ template< typename T >
 class Array
 {
 	private:
-			T _array[];
+			unsigned int	_size;
+			T				*_array;
 	public:
-			Array( void );
+			Array<T>( void )
+			{
+				this->_array = NULL;
+				_size = 0;
+			}
+
+			Array( Array<T> const & rhs )
+			{
+				*this = rhs;
+			}
+
+			Array( unsigned int n )
+			{
+				std::cout << "Parameter constructor" << std::endl;
+				_array = new T[n];
+				_size = n;
+			}
+
+			~Array<T>( void ) { delete [] _array; }
+
+			unsigned int	size() const {
+				return (_size);
+			}
+
+			class OOLE	: public std::exception
+			{
+				public:
+					virtual const char* what() const throw( ) { return ("Exception: Out of the Limits"); }
+			};
+
+			T&			operator[]( int i )
+			{
+				if (i < (int)_size)
+					return (_array[i]);
+				throw OOLE();
+			}
+
+			Array<T>&	operator=( Array<T> const & rhs )
+			{
+				this->_size = rhs._size;
+				this->_array = new T[_size];
+				for (int i = 0 ; i < (int)_size ; i++)
+					this->_array[i] = rhs._array[i];
+				return (*this);
+			}
 };
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include "Array.hpp"
 
 int	main(void)
 {
-	int*	a = new int();
+	std::string* s = new std::string;
 
-	std::cout << *a << std::endl;
+	std::cout << *s << std::endl;
+	Array<unsigned int> test = Array<unsigned int>(5);
+	try
+	{
+		std::cout << test[2] << std::endl;
+		Array<unsigned int> bonsoir = test;
+
+		Array<std::string> strs = Array<std::string>(4);
+		std::cout << strs[2] << std::endl;
+
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
